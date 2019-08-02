7814d14d1868314a49a2221285b89f8a5a8bc917
NeilPhung <neilphung@neilphungs-macbook-pro.local>
2019-08-02 08:28:17 +0700

    Design WelcomeView , connected UI with Code

---
+++ ITChat/WelcomeViewController.swift
@@ -0,0 +1,38 @@
+//
+//  WelcomeViewController.swift
+//  ITChat
+//
+//  Created by NeilPhung on 8/2/19.
+//  Copyright © 2019 NeilPhung. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    
+    
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var repeatPasswordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+    //MARK: IBAction
+    
+    
+    @IBAction func loginButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func registerButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func backgroundTap(_ sender: UITapGestureRecognizer) {
+    }
+    
+
+}
